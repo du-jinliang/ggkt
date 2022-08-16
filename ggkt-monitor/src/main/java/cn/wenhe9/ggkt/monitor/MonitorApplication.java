7a5523dd39950f158eb1d18692c010acd259d2fa
DuJinliang <dujinliang9@163.com>
2022-08-16 18:22:00 +0800

    feat(monitor): 整合nacos提供服务注册与发现、配置中心，整合springboot admin监控平台

---
+++ ggkt-monitor/src/main/java/cn/wenhe9/ggkt/monitor/MonitorApplication.java
@@ -0,0 +1,18 @@
+package cn.wenhe9.ggkt.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DuJinliang
+ * 2022/08/16
+ */
+
+@EnableAdminServer
+@SpringBootApplication
+public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+}
